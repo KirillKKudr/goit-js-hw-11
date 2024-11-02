9ca45f5e9665a018e3b32bc36a96a40dc5e8d959
KirillKKudr <kirillboss2109@gmail.com>
2024-11-02 17:43:07 +0000

    Deploying to gh-pages from @ KirillKKudr/goit-js-hw-11@7eac4ac19d432a5a14e59784d90e3d8b96a7f568 🚀

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
-import{i as l}from"./assets/vendor-I1I71QQ2.js";(function(){const o=document.createElement("link").relList;if(o&&o.supports&&o.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))t(e);new MutationObserver(e=>{for(const r of e)if(r.type==="childList")for(const i of r.addedNodes)i.tagName==="LINK"&&i.rel==="modulepreload"&&t(i)}).observe(document,{childList:!0,subtree:!0});function s(e){const r={};return e.integrity&&(r.integrity=e.integrity),e.referrerPolicy&&(r.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?r.credentials="include":e.crossOrigin==="anonymous"?r.credentials="omit":r.credentials="same-origin",r}function t(e){if(e.ep)return;e.ep=!0;const r=s(e);fetch(e.href,r)}})();const u="46859410-d5a1b7b8d3c8579cf11d58e8b",f="https://pixabay.com/api/";function d(n,o=1,s=12){const t=`${f}?key=${u}&q=${n}&image_type=photo&orientation=horizontal&safesearch=true&page=${o}&per_page=${s}`;return fetch(t).then(e=>{if(!e.ok)throw new Error("Failed to fetch images");return e.json()}).then(e=>e.hits).catch(e=>{throw console.error("Error fetching images:",e),e})}function p(n,o){const s=n.map(t=>`
-    <div class="photo-card">
-      <img src="${t.webformatURL}" alt="${t.tags}" loading="lazy" />
-      <div class="info">
-        <p>Likes: ${t.likes}</p>
-        <p>Views: ${t.views}</p>
-        <p>Comments: ${t.comments}</p>
-        <p>Downloads: ${t.downloads}</p>
-      </div>
-    </div>
-  `).join("");o.innerHTML=s}function c(){l.info({title:"Oops!",message:"Sorry, there are no images matching your search query. Please try again!",position:"topRight",timeout:3e3})}const a=document.querySelector("#search-form"),m=document.querySelector("#gallery"),h=12;let g=1;a.addEventListener("submit",n=>{n.preventDefault();const o=n.currentTarget.elements.searchQuery.value.trim();if(o===""){c();return}d(o,g,h).then(s=>{s.length===0?c():p(s,m),a.reset()}).catch(s=>{console.error("Error processing images:",s)})});
+import{i as p,S as g}from"./assets/vendor-5ObWk2rO.js";(function(){const t=document.createElement("link").relList;if(t&&t.supports&&t.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))i(e);new MutationObserver(e=>{for(const r of e)if(r.type==="childList")for(const n of r.addedNodes)n.tagName==="LINK"&&n.rel==="modulepreload"&&i(n)}).observe(document,{childList:!0,subtree:!0});function o(e){const r={};return e.integrity&&(r.integrity=e.integrity),e.referrerPolicy&&(r.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?r.credentials="include":e.crossOrigin==="anonymous"?r.credentials="omit":r.credentials="same-origin",r}function i(e){if(e.ep)return;e.ep=!0;const r=o(e);fetch(e.href,r)}})();const h="46859410-d5a1b7b8d3c8579cf11d58e8b",y="https://pixabay.com/api/";function b(s,t=1,o=12){const i=`${y}?key=${h}&q=${s}&image_type=photo&orientation=horizontal&safesearch=true&page=${t}&per_page=${o}`;return fetch(i).then(e=>{if(!e.ok)throw new Error("Failed to fetch images");return e.json()}).then(e=>e.hits).catch(e=>{throw console.error("Error fetching images:",e),e})}const a=()=>{p.info({title:"No Results",message:"Sorry, there are no images matching your search query. Please try again!"})},L=(s,t)=>{const o=s.map(({webformatURL:i,largeImageURL:e,tags:r,likes:n,views:d,comments:f,downloads:m})=>`
+      <li class="gallery-item">
+        <a href="${e}">
+          <img src="${i}" alt="${r}" loading="lazy" />
+        </a>
+        <div class="info">
+          <p><b>Likes:</b> ${n}</p>
+          <p><b>Views:</b> ${d}</p>
+          <p><b>Comments:</b> ${f}</p>
+          <p><b>Downloads:</b> ${m}</p>
+        </div>
+      </li>
+    `).join("");t.innerHTML=o},c=document.querySelector(".search-form"),$=document.querySelector(".gallery"),w=12;let S=1,u;const q=()=>{u=new g(".gallery a")},E=()=>{document.querySelector(".loader").style.display="block"},l=()=>{document.querySelector(".loader").style.display="none"};c.addEventListener("submit",s=>{s.preventDefault();const t=s.currentTarget.elements.search.value.trim();if(t===""){a();return}E(),b(t,S,w).then(o=>{l(),o.length===0?a():(L(o,$),q(),u.refresh()),c.reset()}).catch(o=>{l(),console.error("Error processing images:",o),iziToast.error({title:"Error",message:"Failed to load images"})})});
 //# sourceMappingURL=index.js.map
